05e2589c87ece8a907a433483ebbf1e37af9a30c
vikto <vikto@GEMOFVIKTOR>
2024-06-07 13:40:13 +0200

    Add project files.

---
+++ DemoLibrary.Tests/DisplayMessagesTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+// Arrange, Act, Assert
+// expected and actual
+namespace DemoLibrary.Tests
+{
+    public class DisplayMessagesTests
+    {
+        [Fact]
+        public void GreetingShouldReturnGoodMorningMessage()
+        {
+            // Arrange
+            DisplayMessages messages = new DisplayMessages();
+            var expected = "Go to bed viktor";
+
+            // Act
+            var actual = messages.Greeting("viktor", 2);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GreetingShouldReturnGoodAfternoonMessage()
+        {
+            // Arrange
+            DisplayMessages messages = new DisplayMessages();
+            var expected = "Good afternoon viktor";
+
+            // Act
+            var actual = messages.Greeting("viktor",13);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("viktor", 0, "Go to bed viktor")]
+        [InlineData("viktor", 1, "Go to bed viktor")]
+        [InlineData("viktor", 2, "Go to bed viktor")]
+        [InlineData("viktor", 3, "Go to bed viktor")]
+        [InlineData("viktor", 4, "Go to bed viktor")]
+        [InlineData("viktor", 5, "Good morning viktor")]
+        [InlineData("viktor", 6, "Good morning viktor")]
+        [InlineData("viktor", 7, "Good morning viktor")]
+        [InlineData("viktor", 8, "Good morning viktor")]
+        [InlineData("viktor", 9, "Good morning viktor")]
+        [InlineData("viktor", 10, "Good morning viktor")]
+        [InlineData("viktor", 11, "Good morning viktor")]
+        [InlineData("viktor", 12, "Good afternoon viktor")]
+        [InlineData("viktor", 13, "Good afternoon viktor")]
+        [InlineData("viktor", 14, "Good afternoon viktor")]
+        [InlineData("viktor", 15, "Good afternoon viktor")]
+        [InlineData("viktor", 16, "Good afternoon viktor")]
+        [InlineData("viktor", 17, "Good afternoon viktor")]
+        [InlineData("viktor", 18, "Good evening viktor")]
+        [InlineData("viktor", 19, "Good evening viktor")]
+        [InlineData("viktor", 20, "Good evening viktor")]
+        [InlineData("viktor", 21, "Good evening viktor")]
+        [InlineData("viktor", 22, "Good evening viktor")]
+        [InlineData("viktor", 23, "Good evening viktor")]
+        public void GreetingShouldReturnExpectedValue(
+            string firstName,
+            int hourOfTheDay,
+            string expected)
+        {
+            // Arrange
+            DisplayMessages messages = new DisplayMessages();
+            
+            // Act
+            string actual = messages.Greeting(firstName, hourOfTheDay);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ DemoLibrary/DisplayMessages.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Red, Green, Refactor = write a test that turns red then, do whatever you need to do to turn the light green
+// then you refactor
+
+namespace DemoLibrary
+{
+    public class DisplayMessages
+    {
+        public string Greeting(string firstName, int hourOfTheDay)
+        {
+            var output = "";
+            if (hourOfTheDay <= 4)
+            {
+                output = $"Go to bed {firstName}";
+            }
+            else if (hourOfTheDay < 12)
+            {
+                output = $"Good morning {firstName}";
+            }
+            else if (hourOfTheDay < 18)
+            {
+                output = $"Good afternoon {firstName}";
+            }
+            else
+            {
+                output = $"Good evening {firstName}";
+            }
+            return output;
+        }
+    }
+}
